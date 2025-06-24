0c2472ac95c9308a2aef47135a3e41ed426da9b4
Tristan Ross <tristan.ross@midstall.com>
2025-06-23 21:38:02 -0700

    feat: implement head and tail

--- src/Workflow/Graph/Step/Tail.zig
+++ src/Workflow/Graph/Step/Tail.zig
@@ -10,8 +10,46 @@ pub fn deinit(self: *Self, alloc: std.mem.Allocator) void {
 }
 
 pub fn run(self: *Self, alloc: std.mem.Allocator, inputs: *Workflow.InputMap) ![]const u8 {
-    _ = self;
-    _ = alloc;
-    _ = inputs;
-    return error.NotImplemented;
+    const input = try self.input.get(alloc, inputs);
+    defer alloc.free(input);
+
+    const lines = try std.fmt.allocPrint(alloc, "{}", .{self.lines});
+    defer alloc.free(lines);
+
+    var child = std.process.Child.init(&.{"tail", "--", lines}, alloc);
+
+    child.stdin_behavior = .Pipe;
+    child.stderr_behavior = .Pipe;
+    child.stdout_behavior = .Pipe;
+
+    try child.spawn();
+
+    var stdout: std.ArrayListUnmanaged(u8) = .empty;
+    errdefer stdout.deinit(alloc);
+
+    var stderr: std.ArrayListUnmanaged(u8) = .empty;
+    errdefer stderr.deinit(alloc);
+
+    const thread = try std.Thread.spawn(.{
+        .allocator = alloc,
+    }, runThread, .{ &child.stdin, input });
+
+    try child.collectOutput(alloc, &stdout, &stderr, std.math.maxInt(u64));
+
+    const term = try child.wait();
+    thread.join();
+
+    if (term != .Exited) return error.UnexpectedFailure;
+
+    return try stdout.toOwnedSlice(alloc);
+}
+
+fn runThread(stdin: *?std.fs.File, input: []const u8) void {
+    defer {
+        stdin.*.?.close();
+        stdin.* = null;
+    }
+
+    const writer = stdin.*.?.writer();
+    _ = writer.writeAll(input) catch undefined;
 }
